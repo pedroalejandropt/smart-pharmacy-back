8de5a15eb3085b225f088a0e055a6741f9c36fa2
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-01-23 15:25:34 -0400

    ADD: Endpoints to Role Controller

--- Controllers/role_controller.py
+++ Controllers/role_controller.py
@@ -1,8 +1,10 @@
 from flask import Flask, Blueprint, jsonify, request, abort
-from flask_sqlalchemy import SQLAlchemy
-from Models.role import Role
-import Services.role_service as role_service
+from Errors.errors import bad_request, not_found
 from werkzeug.exceptions import HTTPException
+import Services.role_service as role_service
+from flask_sqlalchemy import SQLAlchemy
+from Security.jwt import token_required
+from Models.role import Role
 
 api_role = Blueprint('roles', 'roles')
 
@@ -22,12 +24,19 @@ def api_post():
     role = role_service.post(request.json)
     return jsonify(role.serialize)
 
-def bad_request(message):
-    response = jsonify({'error': message})
-    response.status_code = 400
-    return response
+@api_role.route('/api/v1/roles/<id>', methods=['PUT'])
+@token_required
+def api_put(current_user, id=0):
+    ''' Update entity by id'''
+    body = request.json
+    body['id'] = int(id)
+    print(body)
+    res = role_service.put(body)
+    return jsonify(res.as_dict()) if isinstance(res, Role) else jsonify(res)
 
-def not_found(message):
-    response = jsonify({'error': message})
-    response.status_code = 404
-    return response
+@api_role.route('/api/v1/roles/<id>', methods=['DELETE'])
+@token_required
+def api_delete(current_user, id=0):
+    ''' Delete entity by id'''
+    res = role_service.delete(id)
+    return jsonify(res.as_dict())
