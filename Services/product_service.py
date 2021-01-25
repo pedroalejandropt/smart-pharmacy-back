7e396c5e93aa17a363627ca8c1386a329447ea11
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-01-24 21:52:57 -0400

    ADD: Functions to Manage Products

--- Services/product_service.py
+++ Services/product_service.py
@@ -1,15 +1,23 @@
 from Models.product import Product
+from Models.user_product import User_Product
 from config import db
-""" from werkzeug.exceptions import NotFound """
+from werkzeug.exceptions import NotFound
 
 def get():
     '''
     Get all entities
     :returns: all entity
     '''
-    return Product.query.all()
+    return Product.query.filter_by(delete = False)
 
-def post(body):
+def get_by_id(id):
+    '''
+    Get entitiy
+    :returns: entity
+    '''
+    return Product.query.filter_by(id=id).first()
+
+def post(body, user_body):
     '''
     Create entity with body
     :param body: request body
@@ -18,4 +26,42 @@ def post(body):
     product = Product(**body)
     db.session.add(product)
     db.session.commit()
+
+    user_body['product_id'] = product.id
+
+    user_product = User_Product(**user_body)
+    db.session.add(user_product)
+    db.session.commit()
     return product
+
+def put(body):
+    '''
+    Update entity by id
+    :param body: request body
+    :returns: the updated entity
+    '''
+    product = Product.query.get(body['id'])
+
+    if product:
+        product = Product(**body)
+        db.session.merge(product)
+        db.session.flush()
+        db.session.commit()
+        return product
+    raise NotFound('no such entity found with id=' + str(body['id']))
+
+def delete(id):
+    '''
+    Update entity by id
+    :param body: request body
+    :returns: the updated entity
+    '''
+    product = Product.query.get(id)
+    
+    if product:
+        product.delete = True
+        db.session.merge(product)
+        db.session.flush()
+        db.session.commit()
+        return product
+    raise NotFound('no such entity found with id=' + str(id))
