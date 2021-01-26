25bb53494ba01ca9796d3206d15d0107fe82f4ad
Annemarie Rolo <annemarierolo@gmail.com>
2021-01-25 20:46:31 -0400

    ADD: New Model Exchange

--- Models/exchange.py
+++ Models/exchange.py
@@ -1,14 +1,19 @@
+from config import db
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from setup.db import db
 
 class Exchange(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    delete = db.Column(db.Boolean, nullable=False)
     date = db.Column(db.DateTime(), nullable=False)
     amount = db.Column(db.Float(), nullable=False)
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, date, amount, delete=False, id=None):
+        if id != None:
+            self.id = id
+        self.delete = delete
+        self.date = date
+        self.amount = amount
 
     # Gets dict with the Exchange Object
     @property
@@ -19,3 +24,6 @@ class Exchange(db.Model):
             'date': self.date,
             'amount': self.amount
         }
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
