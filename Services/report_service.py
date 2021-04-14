34be555e572420c6d785cafefef4decbde912891
Annemarie Rolo <annemarierolo@gmail.com>
2021-04-14 16:20:27 -0400

    ADD: Reports Service

---
+++ Services/report_service.py
@@ -0,0 +1,165 @@
+from config import db
+from Models.sale import Sale
+from Models.product import Product
+from werkzeug.exceptions import NotFound
+from Models.product_sale import Product_Sale
+from Services.ml_service import *
+from operator import itemgetter
+from datetime import datetime
+
+months_labels = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
+
+def worst_sellers(month, year):
+    sellers = []
+    result = (Sale.query.join(Product_Sale, Sale.id == Product_Sale.sale_id)
+        .join(Product, Product_Sale.product_id == Product.id)
+        .add_columns(Product.code, Product.name, Sale.year, Sale.month, Sale.salesNumber)
+        .filter(Sale.month == month, Sale.year == year)
+        .order_by(Sale.salesNumber)
+        .order_by(Sale.year)
+        .order_by(Sale.month)
+        .order_by(Product.code)
+        .distinct(Sale.year, Sale.month, Product.code, Sale.salesNumber)
+    ).limit(5).all()
+    for item in result:
+        sellers.append({ "year": item.year, "month": item.month, "product": item.name.title(), "Ventas": item.salesNumber })
+    return sellers
+
+def best_sellers(month, year):
+    sellers = []
+    result = (Sale.query.join(Product_Sale, Sale.id == Product_Sale.sale_id)
+        .join(Product, Product_Sale.product_id == Product.id)
+        .add_columns(Product.code, Product.name, Sale.year, Sale.month, Sale.salesNumber)
+        .filter(Sale.month == month, Sale.year == year)
+        .order_by(Sale.salesNumber.desc())
+        .order_by(Sale.year)
+        .order_by(Sale.month)
+        .order_by(Product.code)
+        .distinct(Sale.year, Sale.month, Product.code, Sale.salesNumber)
+    ).limit(5).all()
+    for item in result:
+        sellers.append({ "year": item.year, "month": item.month, "product": item.name.title(), "Ventas": item.salesNumber })
+    return sellers
+
+def worst_amounts(month, year):
+    amounts = []
+    total = 0
+    result = (Sale.query.join(Product_Sale, Sale.id == Product_Sale.sale_id)
+        .join(Product, Product_Sale.product_id == Product.id)
+        .add_columns(Product.code, Product.name, Sale.year, Sale.month, Sale.salesNumber, (Sale.salesNumber * Product.price).label('amount'))
+        .filter(Sale.month == month, Sale.year == year)
+        .order_by((Sale.salesNumber * Product.price))
+        .order_by(Product.code)
+        .order_by(Sale.year)
+        .order_by(Sale.month)
+        .distinct((Sale.salesNumber * Product.price), Product.code)
+    ).limit(5).all()
+    for item in result:
+        total = total + item.amount
+        amounts.append({ "year": item.year, "month": item.month, "product": item.name.title(), "amount": round(item.amount , 2)})
+    return {"total": total, "data": amounts}
+
+def best_amounts(month, year):
+    amounts = []
+    total = 0
+    result = (Sale.query.join(Product_Sale, Sale.id == Product_Sale.sale_id)
+        .join(Product, Product_Sale.product_id == Product.id)
+        .add_columns(Product.code, Product.name, Sale.year, Sale.month, Sale.salesNumber, (Sale.salesNumber * Product.price).label('amount'))
+        .filter(Sale.month == month, Sale.year == year)
+        .order_by((Sale.salesNumber * Product.price).desc())
+        .order_by(Product.code)
+        .order_by(Sale.year)
+        .order_by(Sale.month)
+        .distinct((Sale.salesNumber * Product.price), Product.code)
+    ).limit(5).all()
+    for item in result:
+        total = total + item.amount
+        amounts.append({ "year": item.year, "month": item.month, "product": item.name.title(), "amount": round(item.amount , 2) })
+    return {"total": total, "data": amounts}
+
+def product_sell_variation_four(code):
+    report = []
+    actual_year = datetime.now().year
+    first_year = actual_year - 3
+    for i in range(first_year, actual_year + 1):
+        result = (Sale.query.join(Product_Sale, Sale.id == Product_Sale.sale_id)
+            .join(Product, Product_Sale.product_id == Product.id)
+            .add_columns(Product.code, Product.name, Sale.year, Sale.month, Sale.salesNumber)
+            .filter(Sale.year == i, Product.code == code)
+            .order_by(Sale.month)
+            .distinct(Sale.month)
+        ).all()
+        if (len(result) == 0):
+            report.append({"year": i, "data": [ { "month": 1, "ventas": 0 }, { "month": 2, "ventas": 0 }, { "month": 3, "ventas": 0 }, { "month": 4, "ventas": 0 }, { "month": 5, "ventas": 0 }, { "month": 6, "ventas": 0 }, { "month": 7, "ventas": 0 }, { "month": 8, "ventas": 0 }, { "month": 9, "ventas": 0 }, { "month": 10, "ventas": 0 }, { "month": 11, "ventas": 0 }, { "month": 12, "ventas": 0 } ]})
+        else:  
+            data = []      
+            for item in result:
+                data.append({ "month": item.month, "ventas": item.salesNumber })
+                #print(item.keys())
+            report.append({"year": i, "data": data})
+    return report
+
+def product_sell_variation(code, year):
+    report = []
+    result = (Sale.query.join(Product_Sale, Sale.id == Product_Sale.sale_id)
+        .join(Product, Product_Sale.product_id == Product.id)
+        .add_columns(Product.code, Product.name, Sale.year, Sale.month, Sale.salesNumber)
+        .filter(Sale.year == year, Product.code == code)
+        .order_by(Sale.month)
+        .distinct(Sale.month)
+    ).all()
+    for item in result:
+        #print(item.keys())
+        report.append({ "month": months_labels[item.month - 1], "ventas": item.salesNumber })
+    return report
+
+## Predictions
+
+def predict_worst_sellers(month, year):
+    sellers = []
+    products = Product.query.filter_by(delete = False)
+    for item in products:
+        sellers.append({ "year": year, "month": month, "product": item.name.title(), "Ventas": predict(int(year), int(month), float(item.code)) })
+    sellers = sorted(sellers, key=itemgetter('Ventas'))
+    """ print(sellers[0:5]) """
+    return sellers[0:5]
+
+def predict_best_sellers(month, year):
+    sellers = []
+    products = Product.query.filter_by(delete = False)
+    for item in products:
+        sellers.append({ "year": year, "month": month, "product": item.name.title(), "Ventas": predict(int(year), int(month), float(item.code)) })
+    sellers = sorted(sellers, key=itemgetter('Ventas'), reverse=True)
+    """ print(sellers[0:5]) """
+    return sellers[0:5]
+
+def predict_worst_amounts(month, year):
+    amounts = []
+    total = 0
+    products = Product.query.filter_by(delete = False)
+    for item in products:
+        ## OJO Aqui va 0 en el else
+        amount = round(predict(int(year), int(month), float(item.code)), 2) if (predict(int(year), int(month), float(item.code))>0) else 1
+        amounts.append({ "year": year, "month": month, "product": item.name.title(), "amount": (amount*item.price) })
+    amounts = sorted(amounts, key=itemgetter('amount'))
+    return {"total": total, "data": amounts[0:5]}
+
+def predict_best_amounts(month, year):
+    amounts = []
+    total = 0
+    products = Product.query.filter_by(delete = False)
+    for item in products:
+        amount = round(predict(int(year), int(month), float(item.code)), 2) if (predict(int(year), int(month), float(item.code))>0) else 0
+        amounts.append({ "year": year, "month": month, "product": item.name.title(), "amount": (amount*item.price) })
+    amounts = sorted(amounts, key=itemgetter('amount'), reverse=True)
+    return {"total": total, "data": amounts[0:5]}
+
+def predict_product_sell_variation(code, year):
+    report = []
+    for index, item in enumerate(months_labels):
+        month = index + 1
+        sales = round(predict(year, month, code), 2) if (predict(year, month, code)>0) else 0
+        report.append({ "month": item, "ventas": sales })
+    return report
+
+
